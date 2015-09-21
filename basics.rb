d0481d9fc8678771bf4b6445c5e8ad6fec54593b
joycedelatorre <joycedelatorre87@gmail.com>
2015-09-21 14:11:44 -0700

    add views

--- basics.rb
+++ basics.rb
@@ -1,4 +1,4 @@
-require 'rubygems'
+# require 'rubygems'
 require 'sinatra'
 
 get '/' do
@@ -8,3 +8,27 @@ end
 get '/about' do
   'A little about me.'
 end
+
+get '/hello/:name' do
+  "Hello there, #{params[:name].upcase}."
+end
+
+get '/hello/:name/:city' do
+  "Hello there, #{params[:name].upcase} from #{params[:city]}."
+end
+
+get '/more/*' do
+  # Anything included in the URL after /more/ will be accessible through the :splat key in the params array.
+  params[:splat]
+  # "Hello, #{params[:splat]}."
+end
+
+get '/form' do
+  erb :form
+end
+
+post '/form' do
+  "You said #{params[:message]}"
+end
+
+
